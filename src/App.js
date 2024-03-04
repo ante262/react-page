90aee34332773db12947b7fef8bbc9a0d58d7171
Ante Ljubić <>
2024-03-04 15:52:00 +0100

    Added iframe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import dashboardImage from './images/dashboard-image.webp';
-import underConstruction from './images/under-construction.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-container">
-        <img src={dashboardImage} className="App-dashboard-image" alt="dashboard" />
-        <div className='App-info'>
-          <h1 className='App-h1'>
-            ROL-BOX
-          </h1>
-          <p className="App-text">
-            Inovativna rješenja za Vaš dom
-          </p>
-        </div>
-        <p>
-          Kontakt: 095 895 4725
-        </p>
-      </div>
-      <img src={underConstruction} className="App-under-construction" alt="under-construction" />
+      <iframe 
+        id="myFrame" 
+        className='App-iframe' 
+        src='https://nikolinasafar18.wixsite.com/my-site-1'
+      />
     </div>
   );
 }
